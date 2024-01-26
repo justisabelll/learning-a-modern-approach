9895afa7ced4e4c670c2b8fcea5d2bf03c2979d2
roy <roywashington1547@gmail.com>
2024-01-25 19:29:57 -0600

    styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,128 @@
-import Image from "next/image";
+import Image from 'next/image';
+
+interface Message {
+  text: string;
+  from: string;
+}
+
+const fakeMessages = [
+  {
+    text: 'Hello',
+    from: 'user',
+  },
+  {
+    text: "Hi, I'm a chatbot you can ask questions about about the Artificial Intelligence: A Modern Approach book.",
+    from: 'ai',
+  },
+  {
+    text: 'What is the book about?',
+    from: 'user',
+  },
+  {
+    text: 'The book is about Artificial Intelligence.',
+    from: 'ai',
+  },
+];
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+    <main className="min-h-screen flex-col items-center p-24">
+      <div className="py-2 text-center">
+        <h1 className="text-3xl ">Learning, A Modern Approach</h1>
+        <h2 className="text-sm">
+          Chat about the{' '}
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            className="underline underline-offset-1 text-blue-500"
+            href="https://aima.cs.berkeley.edu/"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            Artifical Intelligence: A Modern Approach Textbook
           </a>
+        </h2>
+      </div>
+      <ChapterRadioGroup />
+      <div className="">
+        <div className="p-2 rounded-md border">
+          {fakeMessages.map((message) => (
+            <Message
+              key={message.text}
+              from={message.from}
+              text={message.text}
+            />
+          ))}
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <ChatInput />
       </div>
     </main>
   );
 }
+
+const Message = ({ text, from }: Message) => {
+  // it kinda looks cool if you remove the flex
+  return (
+    <div
+      className={`flex flex-col ${from === 'ai' ? 'items-start' : 'items-end'}`}
+    >
+      <div
+        className={`p-2 rounded-xl border ${
+          from === 'ai' ? 'bg-gray-100' : 'bg-violet-00'
+        }`}
+      >
+        {text}
+      </div>
+      <div className="text-xs text-gray-500  ">
+        {from === 'user' ? 'you' : from}
+      </div>
+    </div>
+  );
+};
+
+const ChatInput = () => {
+  return (
+    <div className="flex items-center gap-2 py-2 ">
+      <textarea className="p-2 rounded-md border min-h-[4rem] max-h-56 resize-none overflow-auto w-full" />
+      <button className=" border rounded-md bg-black px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-zinc-800 h-full ">
+        Send
+      </button>
+    </div>
+  );
+};
+
+const ChapterRadioGroup = () => {
+  return (
+    <div className="flex gap-2 mb-2">
+      <div>
+        <div className="flex items-center gap-1">
+          <input type="radio" name="chapter" />
+          <label>Chapter 1</label>
+        </div>
+        <div className="flex items-center gap-1">
+          <input type="radio" name="chapter" />
+          <label>Chapter 2</label>
+        </div>
+      </div>
+      <div>
+        <div className="flex items-center gap-1">
+          <input type="radio" name="chapter" />
+          <label>Chapter 3</label>
+        </div>
+        <div className="flex items-center gap-1">
+          <input type="radio" name="chapter" />
+          <label>Chapter 4</label>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Divider = () => {
+  return (
+    <div className="relative mt-4">
+      <div className="absolute inset-0 flex items-center" aria-hidden="true">
+        <div className="w-full border-t border-gray-300" />
+      </div>
+      <div className="relative flex justify-center">
+        <span className="bg-white px-2 text-sm text-gray-500"></span>
+      </div>
+    </div>
+  );
+};
